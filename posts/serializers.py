91bfbe5711716ed811d0a79d7d774853bf333d32
AmarDange <amardange218@gmail.com>
2023-06-16 06:51:27 +0000

    adding post

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,12 +1,12 @@
 from rest_framework import serializers
-from posts.models import Posts
+from posts.models import Post
 
 
-class PostsSerializer(serializers.ModelSerializer):
+class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
-    posts_id = serializers.ReadOnlyField(source='owner.posts.id')
-    posts_image = serializers.ReadOnlyField(source='owner.posts.image.url')
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
 
     def validate_image(self, value):
         if value.size > 2 * 1024 * 1024:
@@ -26,9 +26,9 @@ class PostsSerializer(serializers.ModelSerializer):
         return request.user == obj.owner
 
     class Meta:
-        model = Posts
+        model = Post
         fields = [
-            'id', 'owner', 'is_owner', 'posts_id',
-            'posts_image', 'created_at', 'updated_at',
+            'id', 'owner', 'is_owner', 'profile_id',
+            'profile_image', 'created_at', 'updated_at',
             'title', 'content', 'image', 'image_filter'
         ]
--- posts/urls.py
+++ posts/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from posts import views
 
 urlpatterns = [
-    path('posts/', views.PostsList.as_view()),
-    path('posts/<int:pk>/', views.PostsDetail.as_view()),
+    path('posts/', views.PostList.as_view()),
 ]
