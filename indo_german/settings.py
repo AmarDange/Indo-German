713edb1a27d4f178f180c9f7df5aa3fb4f4c5bda
AmarDange <amardange218@gmail.com>
2023-06-20 21:04:29 +0000

    changing settings file

--- indo_german/settings.py
+++ indo_german/settings.py
@@ -43,17 +43,17 @@ if 'DEV' not in os.environ:
         'rest_framework.renderers.JSONRenderer',
     ]
 
-# if 'DEV' in os.environ:
-#      DATABASES = {
-#          'default': {
-#              'ENGINE': 'django.db.backends.sqlite3',
-#              'NAME': BASE_DIR / 'db.sqlite3',
-#          }
-#      }
-# else:
-#      DATABASES = {
-#          'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-#      }
+if 'DEV' in os.environ:
+     DATABASES = {
+         'default': {
+             'ENGINE': 'django.db.backends.sqlite3',
+             'NAME': BASE_DIR / 'db.sqlite3',
+         }
+     }
+else:
+     DATABASES = {
+         'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+     }
     #  print('connected')
 
 REST_USE_JWT = True
