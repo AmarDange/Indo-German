b629779dab2b9fc6271712f98a4312aaa86ca06f
AmarDange <>
2023-10-02 10:19:49 +0000

    adding changes comment js

--- frontend/src/pages/comments/Comment.js
+++ frontend/src/pages/comments/Comment.js
@@ -9,72 +9,126 @@ import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { axiosRes } from "../../api/axiosDefaults";
 
 const Comment = (props) => {
-  const {
-    profile_id,
-    profile_image,
-    owner,
-    updated_at,
-    content,
-    id,
-    setPost,
-    setComments,
-  } = props;
+    const {
+        profile_id,
+        profile_image,
+        owner,
+        updated_at,
+        content,
+        id,
+        setPost,
+        setComments,
+        comment_likes_id,
+        comment_likes_count,
+    } = props;
 
-  const [showEditForm, setShowEditForm] = useState(false);
-  const currentUser = useCurrentUser();
-  const is_owner = currentUser?.username === owner;
+    const [showEditForm, setShowEditForm] = useState(false);
+    const currentUser = useCurrentUser();
+    const is_owner = currentUser?.username === owner;
 
-  const handleDelete = async () => {
-    try {
-      await axiosRes.delete(`/comments/${id}/`);
-      setPost((prevPost) => ({
-        results: [
-          {
-            ...prevPost.results[0],
-            comments_count: prevPost.results[0].comments_count - 1,
-          },
-        ],
-      }));
+    const handleDelete = async () => {
+        try {
+            await axiosRes.delete(`/comments/${id}/`);
+            setPost((prevPost) => ({
+                results: [
+                    {
+                        ...prevPost.results[0],
+                        comments_count: prevPost.results[0].comments_count - 1,
+                    },
+                ],
+            }));
 
-      setComments((prevComments) => ({
-        ...prevComments,
-        results: prevComments.results.filter((comment) => comment.id !== id),
-      }));
-    } catch (err) { }
-  };
+            setComments((prevComments) => ({
+                ...prevComments,
+                results: prevComments.results.filter((comment) => comment.id !== id),
+            }));
+        } catch (err) { }
+    };
 
-  return (
-    <>
-      <hr />
-      <Media>
-        <Link to={`/profiles/${profile_id}`}>
-          <Avatar src={profile_image} />
-        </Link>
-        <Media.Body className="align-self-center ml-2">
-          <span className={styles.Owner}>{owner}</span>
-          <span className={styles.Date}>{updated_at}</span>
-          {showEditForm ? (
-            <CommentEditForm
-              id={id}
-              profile_id={profile_id}
-              content={content}
-              profileImage={profile_image}
-              setComments={setComments}
-              setShowEditForm={setShowEditForm}
-            />
-          ) : (
-            <p>{content}</p>
-          )}
-        </Media.Body>
-        {is_owner && !showEditForm && (
-          <MoreDropdown
-            handleEdit={() => setShowEditForm(true)}
-            handleDelete={handleDelete}
-          />
-        )}
-      </Media>
-    </>
-  );
+
+
+    const handleLike = async () => {
+        try {
+            const { data } = await axiosRes.post("/comment_likes/", { comment: id });
+            //   console.log("data",data);
+            setComments((prevComments) => ({
+                ...prevComments,
+                results: prevComments.results.map((comment) => {
+                    return comment.id === id
+                        ? {
+                            ...comment,
+                            comment_likes_count: comment.comment_likes_count + 1,
+                            comment_likes_id: data.id,
+                        }
+                        : comment;
+
+                }),
+            }));
+            //  console.log("Comments likes count", comment_likes_count);
+        } catch (err) {
+            //   console.log(err);
+        }
+    };
+    /**
+     * To unlike a comment by the user who liked it
+     * Sends a request to the API
+     * Decreases likes number by 1
+     */
+    const handleUnlike = async () => {
+        try {
+            await axiosRes.delete(`/comment_likes/${comment_likes_id}`);
+            setComments((prevComments) => ({
+                ...prevComments,
+                results: prevComments.results.map((comment) => {
+                    return comment.id === id
+                        ? {
+                            ...comment,
+                            comment_likes_count: comment.comment_likes_count - 1,
+                            comment_likes_id: null,
+                        }
+                        : comment;
+                }),
+            }));
+        } catch (err) {
+            //   console.log(err);
+        }
+    };
+
+
+
+
+    return (
+        <>
+            <hr />
+            <Media>
+                <Link to={`/profiles/${profile_id}`}>
+                    <Avatar src={profile_image} />
+                </Link>
+                <Media.Body className="align-self-center ml-2">
+                    <span className={styles.Owner}>{owner}</span>
+                    <span className={styles.Date}>{updated_at}</span>
+                    {showEditForm ? (
+                        <CommentEditForm
+                            id={id}
+                            profile_id={profile_id}
+                            content={content}
+                            profileImage={profile_image}
+                            setComments={setComments}
+                            setShowEditForm={setShowEditForm}
+                        />
+                    ) : (
+                        <p>{content}</p>
+                    )}
+                </Media.Body>
+                {is_owner && !showEditForm && (
+                    <MoreDropdown
+                        handleEdit={() => setShowEditForm(true)}
+                        handleDelete={handleDelete}
+                    />
+                )}
+            </Media>
+        </>
+    );
 };
 
 export default Comment;
