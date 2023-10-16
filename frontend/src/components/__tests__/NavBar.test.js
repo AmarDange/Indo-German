2c735a76160bc139a07e47b61c38a31af2645208
AmarDange <>
2023-10-16 09:19:11 +0000

    testing

--- frontend/src/components/__tests__/NavBar.test.js
+++ frontend/src/components/__tests__/NavBar.test.js
@@ -4,45 +4,45 @@ import { CurrentUserProvider } from "../../contexts/CurrentUserContext";
 import NavBar from "../NavBar";
 
 test("renders NavBar", () => {
-  render(
-    <Router>
-      <NavBar />
-    </Router>
-  );
+    render(
+        <Router>
+            <NavBar />
+        </Router>
+    );
 
-  // screen.debug();
-  const signInLink = screen.getByRole("link", { name: "Sign in" });
-  expect(signInLink).toBeInTheDocument();
+    // screen.debug();
+    const signInLink = screen.getByRole("link", { name: "Sign in" });
+    expect(signInLink).toBeInTheDocument();
 });
 
 test("renders link to the user profile for a logged in user", async () => {
-  render(
-    <Router>
-      <CurrentUserProvider>
-        <NavBar />
-      </CurrentUserProvider>
-    </Router>
-  );
+    render(
+        <Router>
+            <CurrentUserProvider>
+                <NavBar />
+            </CurrentUserProvider>
+        </Router>
+    );
 
-  const profileAvatar = await screen.findByText("Profile");
-  expect(profileAvatar).toBeInTheDocument();
+    const profileAvatar = await screen.findByText("Profile");
+    expect(profileAvatar).toBeInTheDocument();
 });
 
 test("renders Sign in and Sign up buttons again on log out", async () => {
-  render(
-    <Router>
-      <CurrentUserProvider>
-        <NavBar />
-      </CurrentUserProvider>
-    </Router>
-  );
+    render(
+        <Router>
+            <CurrentUserProvider>
+                <NavBar />
+            </CurrentUserProvider>
+        </Router>
+    );
 
-  const signOutLink = await screen.findByRole("link", { name: "Sign out" });
-  fireEvent.click(signOutLink);
+    const signOutLink = await screen.findByRole("link", { name: "Sign out" });
+    fireEvent.click(signOutLink);
 
-  const signInLink = await screen.findByRole("link", { name: "Sign in" });
-  const signUpLink = await screen.findByRole("link", { name: "Sign up" });
+    const signInLink = await screen.findByRole("link", { name: "Sign in" });
+    const signUpLink = await screen.findByRole("link", { name: "Sign up" });
 
-  expect(signInLink).toBeInTheDocument();
-  expect(signUpLink).toBeInTheDocument();
+    expect(signInLink).toBeInTheDocument();
+    expect(signUpLink).toBeInTheDocument();
 });
